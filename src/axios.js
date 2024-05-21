c9f7b3b7259121ba5ded14058659a9c5f47cd647
krishimodh <krishimodh@gmail.com>
2024-05-21 23:52:58 +0530

    chore: Update axios base URL to use production server

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+/**
+ * Axios instance for making HTTP requests.
+ *
+ * @type {import("axios").AxiosInstance}
+ */
 const instance = axios.create({
     baseURL: "http://localhost:5000",
     withCredentials: true,
