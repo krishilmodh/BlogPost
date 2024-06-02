0d9bad27c71ca7261bd4b86495ca98176633920a
krishimodh <krishimodh@gmail.com>
2024-06-02 21:58:29 +0530

    chore: Add display for post title and description in BlogPost component

--- src/views/BlogPost.js
+++ src/views/BlogPost.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Box, Heading, Text, Flex, IconButton } from '@chakra-ui/react';
 import { EditIcon, DeleteIcon } from '@chakra-ui/icons';
 
+// BlogPost component for displaying a single blog post
 const BlogPost = ({ post }) => {
   return (
     <Box borderWidth="2px" borderColor="gray.200" borderRadius="lg" shadow="lg" p={6} mb={6} bg="white">
       <Flex justifyContent="space-between" alignItems="center" mb={4}>
-        <Heading as="h1" size="xl" fontWeight="bold">{post.title}</Heading>
+        <Heading as="h1" size="xl" fontWeight="bold">{post.title}</Heading> // Display post title
         <Box>
           <IconButton
             aria-label="Edit post"
@@ -14,21 +15,21 @@ const BlogPost = ({ post }) => {
             mr={2}
             variant="outline"
             colorScheme="blue"
-          />
+          /> // Edit post button
           <IconButton
             aria-label="Delete post"
             icon={<DeleteIcon />}
             variant="outline"
             colorScheme="red"
-          />
+          /> // Delete post button
         </Box>
       </Flex>
       <Text color="gray.600" mb={4}>
         {post.description}
-      </Text>
+      </Text> // Display post description
       {/* Continue with the rest of your component */}
     </Box>
   );
 };
 
-export default BlogPost;
+export default BlogPost; // Exporting BlogPost component
